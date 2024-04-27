71ae2d99673ae39c650986b5f9474cd7af672a5d
Amuuuuuuuu <oupquv@gmail.com>
2024-04-28 01:19:44 +0800

    [Feature] 新增方向動作-2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import '@/assets/index.css'
-import { Application, Assets, Sprite, Texture, AnimatedSprite } from 'pixi.js'
+import { Application, Assets, Sprite, Texture, AnimatedSprite, Text } from 'pixi.js'
 import { framesTemplate } from '@/utils/index.js'
 
 const app = new Application()
@@ -14,6 +14,7 @@ logoSprite.anchor.x = .5
 logoSprite.anchor.y = .5
 app.stage.addChild(logoSprite)
 
+// TODO: 後續改用 sprite sheets (https://pixijs.com/8.x/guides/components/sprite-sheets)
 await Assets.load(new URL('@/assets/freeze.json', import.meta.url).href)
 
 // all images
@@ -29,9 +30,9 @@ const character = {
     stand: [0, 1, 2, 3],
     walk: [4, 5, 6, 7],
     freeze: [8, 9],
-    attack1: [10, 11],
+    attack: [10, 11],
     attack2: [12, 13],
-    jumb_attack: [14, 15, 16],
+    jump_attack: [14, 15, 16],
     attack3: [17, 18, 19],
     run: [20, 21, 22],
     things_walk: [23, 24, 25, 26],
@@ -40,24 +41,24 @@ const character = {
     knock_down: [30, 31, 32, 33, 34],
     knock_up: [35, 36],
     fire: [37, 38],
-    // defense: [],
+    defense: [57],
     // jump: [],
   },
   key: {
-    left: keyboard(37),
-    up: keyboard(38),
-    right: keyboard(39),
-    down: keyboard(40),
-    defense: undefined,
-    attack: undefined,
-    jump: undefined,
+    left: keyboard(74),
+    up: keyboard(73),
+    right: keyboard(76),
+    down: keyboard(188),
+    defense: keyboard(190),
+    attack: keyboard(75),
+    jump: keyboard(32),
   },
   rememberKeyList: []
 }
 for (const [key, arr] of Object.entries(character.action)) {
   character.action[key] = arr.map(item => Texture.from(`rollSequence${item.toString().padStart(4, '0')}.png`))
 }
-const freezeAnimate = new AnimatedSprite(character.action.stand)
+const freezeAnimate = new AnimatedSprite(character.action.defense)
 freezeAnimate.animationSpeed = .1
 freezeAnimate.pivot.set(freezeAnimate._texture.frame.height / 2, freezeAnimate._texture.frame.width / 2)
 freezeAnimate.vx = 0
@@ -117,6 +118,26 @@ character.key.down.release = () => {
   }
 }
 
+character.key.attack.press = () => {
+  freezeAnimate._textures = character.action.attack
+}
+
+character.key.attack.release = () => {
+  if (!character.key.attack.operate) {
+    freezeAnimate._textures = character.action.stand
+  }
+}
+
+character.key.defense.press = () => {
+  freezeAnimate._textures = character.action.defense
+}
+
+character.key.defense.release = () => {
+  if (!character.key.defense.operate) {
+    freezeAnimate._textures = character.action.stand
+  }
+}
+
 function play (delta) {
   freezeAnimate.x += freezeAnimate.vx
   freezeAnimate.y += freezeAnimate.vy
@@ -126,6 +147,7 @@ function keyboard (code) {
   const key = { code, press: undefined, release: undefined, operate: false }
 
   key.handler = (env) => {
+    textDemo.text = `${env.key}${env.key.charCodeAt()}`
     if (env.keyCode === key.code) {
       switch (env.type) {
         case 'keydown':
@@ -161,3 +183,14 @@ app.ticker.add(delta => {
 // 生成角色 JSON
 // framesTemplate()
 
+const textDemo = new Text({ text: '', style: { fontSize: 16 } })
+textDemo.pivot.set(textDemo.height / 2, textDemo.width / 2)
+textDemo.x = app.renderer.width / 2
+textDemo.y = app.renderer.height - 50
+app.stage.addChild(textDemo)
+
+// const buttonDemo = new Button(new Text({ text: 'button' }))
+// buttonDemo.onPress.connect(() => {
+//   console.log('buttonDemo.onPress')
+// })
+// app.stage.addChild(buttonDemo)
