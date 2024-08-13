2e385435b9db7301d88fd5fb89a638c32af0e3a7
Amuuuuuuuu <oupquv@gmail.com>
2024-08-14 02:57:20 +0800

    [Feature] 更新按鍵動作與文件

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 import '@/style/index.css'
 import { Application } from 'pixi.js'
+import Keyboard  from '@/keyboard.js'
+import Player from '@/player.js'
 
-// app
 export const app = new Application()
-await app.init({ width: 460, height: 320, backgroundColor: 0xecedf1, antialias: false })
-document.body.appendChild(app.canvas)
 
-// ticker
-app.ticker.add(delta => {
-  // console.log('delta', delta)
-  // player1.ticker()
-})
+const init = async () => {
+  // app
+  await app.init({ width: 800, height: 600, backgroundColor: 0xecedf1, antialias: false })
+  document.body.appendChild(app.canvas)
+
+  const keyboard = new Keyboard()
+  window.addEventListener('keydown', (env) => keyboard.action(env), false)
+  window.addEventListener('keyup', (env) => keyboard.action(env), false)
+  // window.addEventListener('keypress', keyboard.action.bind(keyboard.action), false)
+
+  const player = new Player(app)
+  player.init()
+
+  // app.ticker.add(_delta => {
+  app.ticker.add(() => {
+    keyboard.update()
+    player.update({ obj: keyboard.get })
+  })
+}
+
+init()
---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+export default {
+  player: [
+    {
+      id: 4,
+      name: '1',
+      top: 'i',
+      left: 'j',
+      down: ',',
+      right: 'l',
+      attack: 'k',
+      jump: 'space',
+      defense: '.'
+    }
+  ]
+}
