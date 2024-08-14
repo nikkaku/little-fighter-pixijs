cff22f1e1961ed7dee9f9dedf5f9a2aed43dd664
Amuuuuuuuu <oupquv@gmail.com>
2024-08-15 01:51:18 +0800

    [Feature] 更新遊戲操作流程與文件 (#10)

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,15 @@
-export default {
-  player: [
-    {
-      id: 4,
-      name: '1',
-      top: 'i',
-      left: 'j',
-      down: ',',
-      right: 'l',
-      attack: 'k',
-      jump: 'space',
-      defense: '.'
-    }
-  ]
-}
+export const ELIMINATE_SECOUND_LIMIT = 0
+
+export const NUMBER_OF_PLAYER = 4
+
+export const PLAYER_KEYBOARD = [
+  { 
+    top: 73,
+    left: 74,
+    down: 188,
+    right: 76,
+    attack: 75,
+    jump: 32,
+    defense: 190
+  }
+]
