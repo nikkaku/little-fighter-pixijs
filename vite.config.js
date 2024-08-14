c695166e8b1512533c8451de89c898492c7e1ea4
Amuuuuuuuu <oupquv@gmail.com>
2024-08-14 10:53:33 +0800

    [Fix] 修正部署錯誤-2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 
 export default defineConfig({
+  base: process.env.NODE_ENV ? '/' : '/little-fighter-pixijs',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
